96dad5827817c998e08226a1de87d51d6d44fb12
Luiz Henrique Moraes <luizhenriquesantosmoraes1@gmail.com>
2025-03-25 13:11:13 -0300

    feature: teste dev

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -23,6 +23,7 @@ const Ad = ({size}:{size:"sm" | "md" | "lg"}) =>{
                 <button className="bg-gray-200 text-gray-500 p-2 text-xs rounded-lg">Learn more</button>
             </div>
         </div>
+        //Teste
     )
 }
 
